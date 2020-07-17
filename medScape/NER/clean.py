7932fe848155f646022cc427980d787190235475
N e n e <makiasagawa@gmail.com>
2020-07-16 21:15:31 -0400

    Add files via upload

--- medScape/NER/clean.py
+++ medScape/NER/clean.py
@@ -6,41 +6,41 @@ import glob
 
 filepath = '/n/data1/hsph/biostat/celehs/yih798/drug-disease/MedScape/MedScape_texts/NER/titles/'
 #folders = os.listdir(filepath)
-folders = ["mayoclinic/"]
-
+folders = ['medicine_txts/', 'medlineplus/', 'mSDManual/', 'pediatrics_txts/', 'surgery_txts/', 'wikipedia/']
 for folder in folders:
     output_file_path = folder.replace("/","")+".txt"
-    with open(output_file_path, "a") as output_file:
-        output_file.write("Filename|Term|Code|SemanticType|isChemical")
+    with open("cui_phe/source/"+output_file_path, "w") as output_file:
+        output_file.write("Filename|Term|Code|SemanticType|isChemical\n")
         i = 1
-        for file in os.listdir(filepath+folder):
-            print(i)
+        files = os.listdir(filepath+folder)
+        files.sort()
+
+        for file in files:
+            # print("%f: %s" %(i/197877, file))
+                
             with open(filepath+folder+file,'r') as f1:
                 term = []
                 CUI = []
                 semanticType = []
                 isChemical = []
 
-                info_list = [file]
+                info_list = [file, "NA", "NA", "NA", "NA"]
                 
                 for line in f1.readlines():
                     if "Term|Code|SemanticType|isChemical" not in line:
                         info = line.split("|")
-                        term.append(info[0])
-                        CUI.append(info[1])
-                        semanticType.append(info[2])
-                        isChemical.append(info[3])
+                        if len(info) == 4:
+                            term.append(info[0])
+                            CUI.append(info[1])
+                            semanticType.append(info[2])
+                            isChemical.append(info[3].replace("\n",""))
 
-                if CUI:
-                    info_list.append(",".join(term))
-                    info_list.append(",".join(CUI))
-                    info_list.append(",".join(semanticType))
-                    info_list.append(",".join(isChemical))
-
-                else:
-                    info_list += ["NA", "NA", "NA", "NA"]
+                            if CUI:
+                                # print(file)
+                                info_list = [file, ",".join(term), ",".join(CUI), ",".join(semanticType), ",".join(isChemical)]
 
                 output_file.write("|".join(info_list))
+                output_file.write("\n")
             i += 1
 
 
